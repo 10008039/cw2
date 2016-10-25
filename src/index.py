4dcd0f9d41dbeec63f5da31c0028d84fcd87a4c9
10008039 <10008039@live.napier.ac.uk>
2016-10-25 21:32:01 +0000

    new dir

--- src/index.py
+++ src/index.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html')
+  return "<strong>this is my website</strong>"
+
+@app.route("/hp/")
+def hp():
+  return "THIS IS PAGE TWooooooooooooO"
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
