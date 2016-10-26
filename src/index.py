e71b90811e496661bb0b7104b67be82f620f9e6b
Fahad Latif <10008039@live.napier.ac.uk>
2016-10-26 04:06:31 +0100

    Update index.py

--- src/index.py
+++ src/index.py
@@ -5,29 +5,7 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route(“/gtr/")
-def gtr():
-  return render_template(‘gtr.html')
 
-@app.route(“/p1")
-def p1():
-  return render_template(‘p1.html')
-
-@app.route(“/ferrari/")
-def ferrari():
-  return render_template(‘ferrari.html')
-
-@app.route(“/lambo/")
-def lambo():
-  return render_template(‘lambo.html')
-
-@app.route(“/veyron/")
-def veyron():
-  return render_template(‘veyron.html')
-
-@app.route(“/p918/")
-def p918():
-  return render_template(‘p918.html')
 
 
 @app.errorhandler(404)
