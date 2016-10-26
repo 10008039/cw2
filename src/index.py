ee36d77523c272e71b0e31cd349324ed860d622d
Fahad Latif <10008039@live.napier.ac.uk>
2016-10-26 03:11:08 +0100

    attempt to create links and debug/error added

--- src/index.py
+++ src/index.py
@@ -5,14 +5,34 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/hp/")
-def hp():
-  return "THIS IS PAGE TWooooooooooooO"
+@app.route(“/gtr/")
+def gtr():
+  return render_template(‘gtr.html')
+
+@app.route(“/p1")
+def p1():
+  return render_template(‘p1.html')
+
+@app.route(“/ferrari/")
+def ferrari():
+  return render_template(‘ferrari.html')
+
+@app.route(“/lambo/")
+def lambo():
+  return render_template(‘lambo.html')
+
+@app.route(“/veyron/")
+def veyron():
+  return render_template(‘veyron.html')
+
+@app.route(“/918/")
+def 918():
+  return render_template(‘918.html')
 
 
-
-
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Couldn’t find the page you requested.", 404
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
- 
+  app.run(host=’0.0.0.0’, debug=True)
