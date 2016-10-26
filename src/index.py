d3266eb9726a79916ab776c6967a15e7fdfc974a
Fahad Latif <10008039@live.napier.ac.uk>
2016-10-26 04:10:43 +0100

    testing

--- src/index.py
+++ src/index.py
@@ -5,9 +5,5 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.errorhandler(404)
-def page_not_found(error):
-  return "Couldn’t find the page you requested.", 404
-
 if __name__ == "__main__":
   app.run(host=’0.0.0.0’, debug=True)
